5343605b0b51b065977a4c1addb6011ae88bf598
sable <sablea2020@gmail.com>
2021-04-26 20:33:48 -0400

    bactldev init commit

---
+++ scripts/bactldev.py
@@ -0,0 +1,11 @@
+import os, subprocess, sys
+import argparse
+
+__organization__ = "blackarch"
+__project__ = "bactldev"
+
+def main():
+    print("placeholder")
+
+if __name__ == "__main__":
+    sys.exit(main())
